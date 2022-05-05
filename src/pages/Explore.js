72968dac85e97eb229163ac9e8404aa95bbd1256
Ramond Falcão <ramondfalcao@gmail.com>
2022-05-05 15:26:19 -0300

    :heavy_check_mark: feat: Tela de Explorar finalizada.
    Co-authored-by: Danillo Gonçalves Batista <danillogoncalves@users.noreply.github.com>
    Co-authored-by: Igor Leal <igorhenriqueleal3@gmail.com>

--- src/pages/Explore.js
+++ src/pages/Explore.js
@@ -1,11 +1,29 @@
 import React from 'react';
+import { useHistory } from 'react-router-dom';
 import Footer from '../components/Footer';
 import Header from '../components/Header';
 
 export default function Explore() {
+  const history = useHistory();
   return (
     <div>
       <Header title="Explore" search={ false } />
+      <section>
+        <button
+          type="button"
+          data-testid="explore-foods"
+          onClick={ () => history.push('/explore/foods') }
+        >
+          Explore Foods
+        </button>
+        <button
+          type="button"
+          data-testid="explore-drinks"
+          onClick={ () => history.push('/explore/drinks') }
+        >
+          Explore Drinks
+        </button>
+      </section>
       <Footer />
     </div>
   );
